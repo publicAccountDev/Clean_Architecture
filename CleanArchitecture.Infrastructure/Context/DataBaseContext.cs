4801ab397c19fcbd5dacb10066e51ccb89df6f56
Amin <86mohamadamin@gmail.com>
2021-02-16 14:14:24 +0330

    Add project files.

---
+++ CleanArchitecture.Infrastructure/Context/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Context
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
