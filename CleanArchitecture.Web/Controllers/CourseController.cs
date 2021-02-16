7f5ea3a1e7782db6e428341dee902d82810c7fc0
Amin <86mohamadamin@gmail.com>
2021-02-16 21:27:31 +0330

    Add Course Controller And Create View

---
+++ CleanArchitecture.Web/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseServices _courseServices;
+        public CourseController(ICourseServices courseServices)
+        {
+            _courseServices = courseServices;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _courseServices.GetCourses();
+            return View(model);
+        }
+    }
+}
