186acf2bdf1fac0bc725e04ae6c32e254378dccf
Amin <86mohamadamin@gmail.com>
2021-02-20 22:01:01 +0330

    Add Home Controller Test

---
+++ CleanArchitecture.Test/HomeControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchitecture.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitecture.Test
+{
+    public class HomeControllerTest
+    {
+        [Trait("Index Test", "Home Controller Test")]
+        [Fact]
+        public void IndexTest()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+
+            Assert.IsType<ViewResult>(result);
+            Assert.NotNull(result);
+        }
+    }
+}
